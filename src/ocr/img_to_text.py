f1fe1c5fafb9fa8c5218f3f1d60919e4dda4fe1b
sunwoo <summerham22@gmail.com>
2024-11-27 15:58:46 +0900

    test

--- src/ocr/img_to_text.py
+++ src/ocr/img_to_text.py
@@ -4,10 +4,11 @@ from bs4 import BeautifulSoup
 import certifi
 from ocr.get_img import ocr
 from pymongo import MongoClient
+import os
 
 s3 = boto3.client('s3',
-    aws_access_key_id=$AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=$AWS_SECRET_ACCESS_KEY,
+    aws_access_key_id=os.getenv(AWS_ACCESS_KEY_ID),
+    aws_secret_access_key=os.getenv(AWS_SECRET_ACCESS_KEY),
     region_name="ap-northeast-2"
     )
 
