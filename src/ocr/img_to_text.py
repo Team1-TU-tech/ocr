af1b044059f4f61df3e12d130a54f7a59f2ecd5c
sunwoo <summerham22@gmail.com>
2024-11-27 16:07:07 +0900

    test

--- src/ocr/img_to_text.py
+++ src/ocr/img_to_text.py
@@ -7,8 +7,8 @@ from pymongo import MongoClient
 import os
 
 s3 = boto3.client('s3',
-    aws_access_key_id=os.getenv(AWS_ACCESS_KEY_ID),
-    aws_secret_access_key=os.getenv(AWS_SECRET_ACCESS_KEY),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name="ap-northeast-2"
     )
 
